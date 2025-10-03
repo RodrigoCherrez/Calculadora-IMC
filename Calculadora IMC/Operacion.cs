9bcbfeb8ba5a73b57bfdf39290d4b363af3fcb9f
Rodrigo Sanchez Cherrez <24470207@campeche.tecnm.mx>
2025-10-02 23:07:37 -0600

    Se agrega todos los documentos que no subi

---
+++ Calculadora IMC/Operacion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_IMC
+{
+    internal class Operacion
+    {
+        double Peso;
+        double altura;
+
+        public Operacion(double Pesoo, double alturaa) 
+        {
+            this.Peso = Pesoo;
+            this.altura = alturaa;
+        }
+
+        
+        public double Resultado() 
+        {
+           return Peso / (altura * altura);
+        }
+        public string Info()
+        {
+            double img = Resultado();
+            if (img < 18.5) return "Peso Bajo";
+            else if (img < 25) return "Peso Normal";
+            else if (img < 30) return "Sobrepeso";
+            else if (img < 40) return "Obsesidad";
+            else return "Obesidad extrema";
+        }
+    }
+}
---
+++ Calculadora IMC/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_IMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido a la Calculadora de IMC");
+            Console.WriteLine("Para estar en la misma sintonia se agregan los ejemplos de como debe colocar los datos: Altura 1.79, Peso: 75.3");
+            Console.WriteLine("Ingrese su Peso");
+            double peso = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese su Altura");
+            double altura = double.Parse(Console.ReadLine());
+
+            Operacion op = new Operacion(peso, altura);
+            double img = op.Resultado();
+            Console.WriteLine($" Tu IMC es : {img}");
+            Console.WriteLine($"Estás en : {op.Info()}");
+
+
+        }   
+    }
+}
